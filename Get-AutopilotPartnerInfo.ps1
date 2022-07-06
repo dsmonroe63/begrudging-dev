afc944a94f13f31b27612a06449c2b39540af0a1
dsmonroe63 <98057258+dsmonroe63@users.noreply.github.com>
2022-07-06 10:57:09 -0400

    Add files via upload

---
+++ Get-AutopilotPartnerInfo.ps1
@@ -0,0 +1,35 @@
+﻿<### Get-AutopilotPartnerInfo by Sam Monroe
+
+Partner Center allows Microsoft Partners to upload existing devices into Autopilot without gathering the hardware hash. I have found that getting the hash is a bit
+of a challenge for our MSP for various reasons. This creates a simple .csv that can be uploaded to each of your customers Autopilot environment.
+
+###>
+
+function Get-AutopilotPartnerInfo {
+
+[Cmdletbinding()]
+
+param (
+    [Parameter(Mandatory)]
+    [string]$Path
+    )
+
+$BIOSInfo = Get-CimInstance -ClassName Win32_BIOS
+$ModelInfo = Get-CimInstance -ClassName Win32_ComputerSystem
+
+$AutopilotDevices = @(
+
+[pscustomobject]@{
+
+"Device serial number" = $BIOSInfo.SerialNumber
+"Windows product ID" = $null
+"Hardware hash" = $null
+"Manufacturer name" = $BIOSInfo.Manufacturer
+"Device Model" = $ModelInfo.Model
+
+}
+)
+
+$AutopilotDevices | Export-Csv -Path $Path\AutopilotMachine.csv -NoTypeInformation
+
+}
