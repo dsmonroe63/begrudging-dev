879ad86cf2357ddf7467fc2e981208423af20920
dsmonroe63 <98057258+dsmonroe63@users.noreply.github.com>
2022-07-06 11:01:21 -0400

    Update Get-AutopilotPartnerInfo.ps1

--- Get-AutopilotPartnerInfo.ps1
+++ Get-AutopilotPartnerInfo.ps1
@@ -1,7 +1,8 @@
 ﻿<### Get-AutopilotPartnerInfo by Sam Monroe
 
 Partner Center allows Microsoft Partners to upload existing devices into Autopilot without gathering the hardware hash. I have found that getting the hash is a bit
-of a challenge for our MSP for various reasons. This creates a simple .csv that can be uploaded to each of your customers Autopilot environment.
+of a challenge for our MSP for various reasons. This creates a simple .csv that can be uploaded to each of your customers Autopilot environment. Just enter the Path 
+parameter. This file can be hosted on a network share and run from a device management solution (GPO, SCCM, etc.) or just run on an individual machine.
 
 ###>
 
